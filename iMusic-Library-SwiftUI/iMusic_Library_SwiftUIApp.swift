be13804aff4bb8f07e451f710ba76068dd44517d
Samirlb <samirlora0@gmail.com>
2024-01-31 11:40:01 -0500

    Initial Commit

---
+++ iMusic-Library-SwiftUI/iMusic_Library_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  iMusic_Library_SwiftUIApp.swift
+//  iMusic-Library-SwiftUI
+//
+//  Created by Samir on 31/01/24.
+//
+
+import SwiftUI
+
+@main
+struct iMusic_Library_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
